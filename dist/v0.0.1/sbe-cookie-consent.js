9dc05080d9b618ff3d2f34475ec2f70e5bca30b9
tomruffhightech <tomruffhightech@gmail.com>
2022-07-19 14:12:10 -0600

    seeing if this will work

--- dist/v0.0.1/sbe-cookie-consent.js
+++ dist/v0.0.1/sbe-cookie-consent.js
@@ -4,12 +4,16 @@ sbe-cookie-consent v0.0.1
 Ruff Web Development
 MIT License
 */
-;$(function(){
+$(function () {
     "use strict";
-    function checkCDN (){
+    const checkCDN = () => {
         console.log('cdn working')
     }
+    function chec() {
+        console.log('function 2')
+    }
     // on load
-        checkCDN()
+    checkCDN()
+    chec()
     // end of cdn script
 });
